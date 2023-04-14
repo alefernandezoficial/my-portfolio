254ad4e8c0a32145edaa874a2377e078e6fc1d45
alefernandezoficial <alefernandezoficial@gmail.com>
2023-04-14 10:25:53 -0300

    Updated

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
         <Navbar />
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/portfolio" element={<Portfolio />} />
