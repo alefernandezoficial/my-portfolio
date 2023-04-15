c4f11504c7458e4e223726e3e4363eef54a656a3
alefernandezoficial <alefernandezoficial@gmail.com>
2023-04-15 17:45:15 -0300

    Updated

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../../styles/pages/About.css";
-import Profile from "../../assets/images/profile.jpg";
 
 import htmlIcon from "../../assets/icons/html5.png";
 import cssIcon from "../../assets/icons/CSS5.png";
@@ -9,65 +8,85 @@ import reactIcon from "../../assets/icons/Reactjs.png";
 import bootIcon from "../../assets/icons/bootstrap.png";
 import wordIcon from "../../assets/icons/wordpress.png";
 
-
 function About() {
   return (
     <div className="container">
       <h1 className="about-title">About Me</h1>
-      <img src={Profile} alt="profile" className="imagen-profile" />
-      <p>
-        Hola, me llamo Alejandro Fernandez y soy desarrollador, diseñador y
-        programador.
-      </p>
-      <p>
-        En mi tiempo libre disfruto de aprender nuevos lenguajes de
-        programacion. Siempre con ideas distinta,s para mejorar el rendimiento
-        de mis paginas web.
-      </p>
-      <p>
-        Si estas interesado en trabajar conmigo, no dudes en enviarme un email o
-        contactarme por medio de mis redes sociales o por aqui mismo en la
-        seccion Contacto.
-      </p>
+      <div className="container-info-profile">
+        <p>
+          Hola, me llamo Alejandro Fernandez y soy desarrollador, diseñador y
+          programador.
+
+          En mi tiempo libre disfruto de aprender nuevos lenguajes de
+          programacion. Siempre con ideas distinta,s para mejorar el rendimiento
+          de mis paginas web.
+
+          Si estas interesado en trabajar conmigo, no dudes en enviarme un email
+          o contactarme por medio de mis redes sociales o por aqui mismo en la
+          seccion Contacto.
+        </p>
+      </div>
       <hr />
 
-    <div className="card-container">
-      <div class="card-about">
-            <div class="icon"><img src={htmlIcon} alt="logohtml" className="imgIcon"></img></div>
-            <p class="title">HTML</p>
-            <p class="text">HyperText Markup Language</p>
-      </div>
+      <div className="card-container">
+        <div class="card-about">
+          <div class="icon">
+            <img src={htmlIcon} alt="logohtml" className="imgIcon"></img>
+          </div>
+          <p class="title">HTML</p>
+          <p class="text">HyperText Markup Language</p>
+        </div>
 
-      <div class="card-about">
-            <div class="icon"><img src={cssIcon} alt="logohtml" className="imgIcon"></img></div>
-            <p class="title">CSS</p>
-            <p class="text">Cascading Style Sheets</p>
-      </div>
+        <div class="card-about">
+          <div class="icon">
+            <img src={cssIcon} alt="logohtml" className="imgIcon"></img>
+          </div>
+          <p class="title">CSS</p>
+          <p class="text">Cascading Style Sheets</p>
+        </div>
 
-      <div class="card-about">
-            <div class="icon"><img src={javaIcon} alt="logohtml" className="imgIcon"></img></div>
-            <p class="title">JavaScript</p>
-            <p class="text">lightweight, interpreted, or just-in-time compiled programming language</p>
-      </div>
+        <div class="card-about">
+          <div class="icon">
+            <img src={javaIcon} alt="logohtml" className="imgIcon"></img>
+          </div>
+          <p class="title">JavaScript</p>
+          <p class="text">
+            lightweight, interpreted, or just-in-time compiled programming
+            language
+          </p>
+        </div>
 
-      <div class="card-about">
-            <div class="icon"><img src={reactIcon} alt="logohtml" className="imgIcon"></img></div>
-            <p class="title">ReactJS</p>
-            <p class="text">front-end JavaScript library for building user interfaces based on components</p>
-      </div>
+        <div class="card-about">
+          <div class="icon">
+            <img src={reactIcon} alt="logohtml" className="imgIcon"></img>
+          </div>
+          <p class="title">ReactJS</p>
+          <p class="text">
+            front-end JavaScript library for building user interfaces based on
+            components
+          </p>
+        </div>
 
-      <div class="card-about">
-            <div class="icon"><img src={bootIcon} alt="logohtml" className="imgIcon"></img></div>
-            <p class="title">BootStrap</p>
-            <p class="text"> front-end development framework for the creation of websites and web apps</p>
-      </div>
+        <div class="card-about">
+          <div class="icon">
+            <img src={bootIcon} alt="logohtml" className="imgIcon"></img>
+          </div>
+          <p class="title">BootStrap</p>
+          <p class="text">
+            {" "}
+            front-end development framework for the creation of websites and web
+            apps
+          </p>
+        </div>
 
-      <div class="card-about">
-            <div class="icon"><img src={wordIcon} alt="logohtml" className="imgIcon"></img></div>
-            <p class="title">Wordpress</p>
-            <p class="text">website creation platform</p>
+        <div class="card-about">
+          <div class="icon">
+            <img src={wordIcon} alt="logohtml" className="imgIcon"></img>
+          </div>
+          <p class="title">Wordpress</p>
+          <p class="text">website creation platform</p>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import '../../styles/pages/Home.css';
+import Profile from "../../assets/images/profile-entidad.png";
+import Cover from "../../assets/images/profile-cover.png";
+import Nombre from "../../assets/images/alejandro-fernandez.png";
+
+import "../../styles/pages/Home.css";
 
 function Home() {
   return (
-    <div className='container'>
-      <h1 className='home-title'>Home</h1>
-      <p className='home-parrafo'>
-        Aca podras encontrar toda la informacion de mis conocimientos
-        tanto como Programador como Diseñador.
-        Tengo conocimientos de Front-End HTML, CSS, JavaScript, BootStrap, ReactJS
-        SupaBase y MySQL como base de datos en Back-End.
+    <div className="container">
+      <h1 className="home-title">Home</h1>
+
+      <div className="container-card-presentation">
+        <div class="card-profile">
+          <div class="wrapper">
+            <img src={Cover} class="cover-image" alt="coverImg" />
+          </div>
+          <img src={Nombre} class="title" alt="nameImg"/>
+          <img src={Profile} class="character" alt="profileImg" />
+        </div>
+      </div>
+
+      <p className="home-parrafo">
+        Aca podras encontrar toda la informacion de mis conocimientos tanto como
+        Programador como Diseñador. Tengo conocimientos de Front-End HTML, CSS,
+        JavaScript, BootStrap, ReactJS SupaBase y MySQL como base de datos en
+        Back-End.
       </p>
     </div>
-    
-  )
+  );
 }
 
-export default Home
+export default Home;
