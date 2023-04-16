9d693c79f91c5495277d73a8c3556fbafdc0dd0d
alefernandezoficial <alefernandezoficial@gmail.com>
2023-04-16 04:12:09 -0300

    Updated

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -16,16 +16,31 @@ function Home() {
           <div class="wrapper">
             <img src={Cover} class="cover-image" alt="coverImg" />
           </div>
-          <img src={Nombre} class="title" alt="nameImg"/>
+          <img src={Nombre} class="title" alt="nameImg" />
           <img src={Profile} class="character" alt="profileImg" />
         </div>
       </div>
 
       <p className="home-parrafo">
-        Aca podras encontrar toda la informacion de mis conocimientos tanto como
-        Programador como Diseñador. Tengo conocimientos de Front-End HTML, CSS,
-        JavaScript, BootStrap, ReactJS SupaBase y MySQL como base de datos en
-        Back-End.
+        Buenos días/tardes/noches, Me complace presentarles mis servicios como
+        programador, diseñador y desarrollador web. Con experiencia en HTML,
+        CSS, JavaScript, ReactJS, Bootstrap, Photoshop, Canva, Wordpress
+        SupaBase y MySql, puedo ayudarle a crear una presencia en línea efectiva
+        y atractiva para su empresa o proyecto. Mi objetivo es proporcionar
+        soluciones web personalizadas que satisfagan sus necesidades y objetivos
+        únicos. Con una atención meticulosa a los detalles, garantizo una
+        experiencia de usuario fluida y optimizada que resultará en una mayor
+        retención y conversión de clientes.
+        <hr/>
+        Además de mis habilidades en diseño
+        y desarrollo web, también tengo experiencia en producción musical y
+        audiovisual con Reaper y SonyVegas. Esta experiencia me ha permitido
+        tener una comprensión profunda de la importancia de la calidad del
+        sonido y la imagen en la creación de contenido multimedia. Si está
+        buscando un profesional de confianza y dedicado para su proyecto web, no
+        dude en ponerse en contacto conmigo. Estoy comprometido con la
+        excelencia en cada proyecto que emprendo y estoy listo para ayudarlo a
+        alcanzar sus objetivos. ¡Gracias!
       </p>
     </div>
   );
